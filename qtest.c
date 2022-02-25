bdbe4f84201ae9987ee1f01a78ad8366342b0e9a
hbx <hbx1196@gmail.com>
2022-02-26 00:57:41 +0800

    Implement shuffle command in qtest
    It can used to shuffle nodes in queue with current time as random seed
    of srand.

--- qtest.c
+++ qtest.c
@@ -762,6 +762,41 @@ static bool do_show(int argc, char *argv[])
     return show_queue(0);
 }
 
+static bool do_shuffle(int argc, char *argv[])
+{
+    if (argc != 1) {
+        report(1, "%s takes no arguments", argv[0]);
+        return false;
+    }
+
+    if (!l_meta.l)
+        report(3, "Warning: Try to access null queue");
+    error_check();
+
+    set_noallocate_mode(true);
+
+    srand(time(NULL));
+    struct list_head *head = l_meta.l->next;
+    int sz = q_size(head);
+
+    if (!head)
+        return false;
+    while (sz) {
+        struct list_head *cur = head->next;
+        int pick = rand() % sz;
+
+        for (int i = 0; i < pick; i++)
+            cur = cur->next;
+        list_del(cur);
+        list_add_tail(cur, head);
+        sz--;
+    }
+    set_noallocate_mode(false);
+
+    show_queue(3);
+    return !error_check();
+}
+
 static void console_init()
 {
     ADD_COMMAND(new, "                | Create new queue");
@@ -795,6 +830,7 @@ static void console_init()
         dedup, "                | Delete all nodes that have duplicate string");
     ADD_COMMAND(swap,
                 "                | Swap every two adjacent nodes in queue");
+    ADD_COMMAND(shuffle, "                | Shuffle nodes in queue");
     add_param("length", &string_length, "Maximum length of displayed string",
               NULL);
     add_param("malloc", &fail_probability, "Malloc failure probability percent",
